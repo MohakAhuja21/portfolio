c56aa815af0ddf23a1ab4412013ddb7ef1ddc642
Mohak Ahuja <mohakaahuaj@outlook.com>
2022-07-26 23:10:39 +0530

    download btn now working properly

--- index.js
+++ index.js
@@ -15,15 +15,20 @@ function toggleSidebar(){
 // script for animating download CV button
 let button = document.querySelector(".button");
 
-button.addEventListener("click",()=>{
+
+button.addEventListener("click",()=>{ 
     button.classList.add("active");
 
     setTimeout(()=>{
         button.classList.remove("active");
         document.querySelector("i").classList.replace('bx-cloud-download','bx-check-double');
         document.querySelector(".btn-text").innerHTML="completed";
+        // download will start automatically after animation will be done !
+        let download_href="source/CV/blank pdf.pdf";
+        window.location.href=download_href;
     },6000)//it will remove active button class after 6s.
 })
+
 
 // changing colors by pressing button
 // function changeColor(e){
