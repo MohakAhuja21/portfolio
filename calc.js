4905d3618d7201621aa8e68c0d8c6c7c2b3b48cf
Mohak Ahuja <mohakaahuaj@outlook.com>
2022-08-03 15:07:09 +0530

    minor changes in calculator.html

---
+++ calc.js
@@ -0,0 +1,10 @@
+let op=document.getElementById('output');
+// function to display all the numbers
+function display(num){
+    // output screen display numbers
+    op.value+=num;
+}
+// function for the equal button
+function calculate(){
+    
+}
