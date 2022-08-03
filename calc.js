e2f784ff49a07ddfdc4ae758e5922551a6cdc104
Mohak Ahuja <mohakaahuaj@outlook.com>
2022-08-03 16:56:16 +0530

    done with calculator project

--- calc.js
+++ calc.js
@@ -4,7 +4,21 @@ function display(num){
     // output screen display numbers
     op.value+=num;
 }
-// function for the equal button
+// function for the equal button using try and catch statement
 function calculate(){
-    
+    try{
+        op.value=eval(op.value);
+    }
+    catch(err){
+        alert("don't input more than one symbol");
+    }
+}
+// function for clear button
+function clear(){
+    // when i click on clear button/cl everything must be empty!
+    op.value="";
+}
+function del(){
+    // when we click on delete button it will delete numbers one by one.
+    op.value=op.value.slice(0,-1);
 }
